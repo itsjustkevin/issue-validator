7f60fc60a3cac96cf5dcbf9adccabc486c7151a5
Kevin Chisholm <kevinjchisholm@google.com>
2023-06-05 09:34:52 -0500

    validate json

--- issue_validator.js
+++ issue_validator.js
@@ -23,4 +23,63 @@ function parseGithubIssueMarkdown(body) {
   return json;
 }
 
-module.exports = { parseGithubIssueMarkdown }
+/**
+ * Validates a parsed GitHub issue JSON object for any missing or invalid values.
+ * @param {Object} json - The parsed GitHub issue JSON object.
+ * @returns {string[]} - An array of error messages for any missing or invalid values.
+ */
+function validateGithubIssue(json) {
+  const errors = [];
+
+  // Check for missing or empty values for each key
+  if (!json.issue_link || json.issue_link.trim() === '') {
+    errors.push('Missing or empty value for "issue_link".');
+  }
+
+  if (!json['Commit Hash'] || json['Commit Hash'].trim() === '') {
+    errors.push('Missing or empty value for "Commit Hash".');
+  }
+
+  if (!json.Target || json.Target.trim() === '') {
+    errors.push('Missing or empty value for "Target".');
+  }
+
+  if (!json.pr_link || json.pr_link.trim() === '') {
+    errors.push('Missing or empty value for "pr_link".');
+  }
+
+  if (!json['Changelog Description'] || json['Changelog Description'].trim() === '') {
+    errors.push('Missing or empty value for "Changelog Description".');
+  }
+
+  if (!json['Impacted Users'] || json['Impacted Users'].trim() === '') {
+    errors.push('Missing or empty value for "Impacted Users".');
+  }
+
+  if (!json['Impact Description'] || json['Impact Description'].trim() === '') {
+    errors.push('Missing or empty value for "Impact Description".');
+  }
+
+  if (!json.Workaround || json.Workaround.trim() === '') {
+    errors.push('Missing or empty value for "Workaround".');
+  }
+
+  if (!json.Risk || json.Risk.trim() === '') {
+    errors.push('Missing or empty value for "Risk".');
+  }
+
+  if (!json['Test Coverage'] || json['Test Coverage'].trim() === '') {
+    errors.push('Missing or empty value for "Test Coverage".');
+  }
+
+  if (!json['Validation Steps'] || json['Validation Steps'].trim() === '') {
+    errors.push('Missing or empty value for "Validation Steps".');
+  }
+
+  return errors;
+}
+
+module.exports = {
+  parseGithubIssueMarkdown,
+  validateGithubIssue
+}
--- issue_validator.test.js
+++ issue_validator.test.js
@@ -1,4 +1,4 @@
-const { parseGithubIssueMarkdown } = require('./issue_validator');
+const { parseGithubIssueMarkdown, validateGithubIssue } = require('./issue_validator');
 
 describe('Parse github issue markdown', () => {
   test('should parse GitHub issue body with markdown headings into JSON', () => {
@@ -8,6 +8,53 @@ describe('Parse github issue markdown', () => {
     expect(parsedJson).toEqual(expectedJson);
   })
 })
+
+describe('validateGithubIssue', () => {
+  test('should return an empty array for a valid GitHub issue', () => {
+    const issue = {
+      issue_link: 'https://github.com/flutter/flutter/issues/126403',
+      'Commit Hash': '31a665c',
+      Target: 'stable',
+      pr_link: 'https://github.com/flutter/flutter/pull/126963',
+      'Changelog Description': 'Adds [`namespace` configuration](https://developer.android.com/build/configure-app-module#set-namespace) to module `build.gradle` file templates that is required when using >= Gradle 8.0.',
+      'Impacted Users': 'Flutter devs using add-to-app on Android wanting to use Gradle 8.0+',
+      'Impact Description': 'Flutter developers generating Flutter modules to use for android-add-to-app with an Android app using Gradle 8.0+ will receive a vague error describing an issue with `namespace` in its `build.gradle` configuration files as described in https://github.com/flutter/flutter/issues/126403#issue-1703425186.',
+      Workaround: 'A developer may work around the issue by adding the `namespace` configuration manually to the module\'s `build.gradle` configuration files.',
+      Risk: 'low',
+      'Test Coverage': 'yes',
+      'Validation Steps': 'Once you checkout my fix, you need to find an existing Android app or create one to use for testing. Modify it if necessary to use Gradle 8.0+. Then, create a Flutter module to add to an existing Android app using the command listed [here](https://docs.flutter.dev/add-to-app/android/project-setup#create-a-flutter-module). Finally, try running the app or building the apk and verify that it is successful.'
+    };
+
+    const errors = validateGithubIssue(issue);
+
+    expect(errors).toEqual([]);
+  });
+
+  test('should return an array of error messages for an invalid GitHub issue', () => {
+    const issue = {
+      issue_link: '',
+      'Commit Hash': '',
+      Target: 'stable',
+      pr_link: '',
+      'Changelog Description': 'Adds [`namespace` configuration](https://developer.android.com/build/configure-app-module#set-namespace) to module `build.gradle` file templates that is required when using >= Gradle 8.0.',
+      'Impacted Users': '',
+      'Impact Description': 'Flutter developers generating Flutter modules to use for android-add-to-app with an Android app using Gradle 8.0+ will receive a vague error describing an issue with `namespace` in its `build.gradle` configuration files as described in https://github.com/flutter/flutter/issues/126403#issue-1703425186.',
+      Workaround: 'A developer may work around the issue by adding the `namespace` configuration manually to the module\'s `build.gradle` configuration files.',
+      Risk: 'low',
+      'Test Coverage': 'yes',
+      'Validation Steps': 'Once you checkout my fix, you need to find an existing Android app or create one to use for testing. Modify it if necessary to use Gradle 8.0+. Then, create a Flutter module to add to an existing Android app using the command listed [here](https://docs.flutter.dev/add-to-app/android/project-setup#create-a-flutter-module). Finally, try running the app or building the apk and verify that it is successful.'
+    };
+
+    const errors = validateGithubIssue(issue);
+
+    expect(errors).toEqual([
+      'Missing or empty value for "issue_link".',
+      'Missing or empty value for "Commit Hash".',
+      'Missing or empty value for "pr_link".',
+      'Missing or empty value for "Impacted Users".'
+    ]);
+  });
+});
 
 const issueBody = `
 ### issue_link
